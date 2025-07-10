ca05f56754faf5c7663f0b76ced9b2d7f4c84bb5
LJJ-LUO <38369214+LJJ-LUO@users.noreply.github.com>
2025-07-10 14:02:41 +0800

    Initial Commit

---
+++ Thank_L/Thank_LApp.swift
@@ -0,0 +1,17 @@
+//
+//  Thank_LApp.swift
+//  Thank_L
+//
+//  Created by L on 2025/7/10.
+//
+
+import SwiftUI
+
+@main
+struct Thank_LApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Thank_LTests/Thank_LTests.swift
@@ -0,0 +1,16 @@
+//
+//  Thank_LTests.swift
+//  Thank_LTests
+//
+//  Created by L on 2025/7/10.
+//
+
+import Testing
+
+struct Thank_LTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
